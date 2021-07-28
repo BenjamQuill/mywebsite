4f1521cb92b5d1ddd2c7e49a257e79b162428f3c
BenjamQuill <benjamin.dujarrier@gmail.com>
2021-07-28 13:24:27 +0000

    change linkMe

--- pages/components/LinkMe.js
+++ pages/components/LinkMe.js
@@ -2,18 +2,14 @@ import Link from 'next/link';
 
 export default function LinkMe({slug, title, description}) {
   return (
-    <Link 
-      key={slug} 
-      href={slug}
-      >
         <a
           className="w-full p-3 mt-3 text-black font-bold text-left border rounded-xl hover:text-green-600 focus:text-green-600 md:p-4 md:w-1/5 md:m-1"
+          href={slug}
         >
           <h3 className="text-xl md:text-2xl">&rarr; {title}</h3>
           <p className="text-center mt-4 text-xs text-white text-opacity-75 md:text-xl md:mt-10">
             {description}
           </p>
         </a>
-    </Link>
   )
 }
